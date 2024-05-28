9cffc1e9b4d01400d09059769d6ecd8e5fb7bd1f
Hasmik-icml <gevorgyan.icml@gmail.com>
2024-05-28 22:26:35 +0400

    change migration

--- prisma/migrations/20240528173122_init/migration.sql
+++ prisma/migrations/20240528173122_init/migration.sql
@@ -1,16 +1,16 @@
 -- CreateTable
-CREATE TABLE "Words" (
+CREATE TABLE "Vocabulary" (
     "id" SERIAL NOT NULL,
     "english" TEXT NOT NULL,
     "armenian" TEXT[],
-    "transcription" TEXT NOT NULL,
-    "status" JSONB NOT NULL,
+    "transcription" TEXT,
+    "status" JSONB,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "deletedAt" TIMESTAMP(3),
 
-    CONSTRAINT "Words_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Vocabulary_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Words_english_key" ON "Words"("english");
+CREATE UNIQUE INDEX "Vocabulary_english_key" ON "Vocabulary"("english");
